a7e0cf4036d232c481765760fe9ac2004eaba299
Andreas Appelros <andreas.appelros@gmail.com>
2022-12-01 06:12:58 +0100

    Day 1

--- AoC2022/Day1.cs
+++ AoC2022/Day1.cs
@@ -7,13 +7,67 @@ public class Day1 : AoCTestClass
     public void Part1()
     {
         var input = DayInput();
-        TestContext.WriteLine($"{"result"}");
+        var stringReader = new StringReader(input);
+        var max = 0;
+        var current = 0;
+        while (stringReader.ReadLine() is { } line)
+        {
+            if (line == "")
+            {
+                max = Math.Max(current, max);
+                current = 0;
+                continue;
+            }
+            var value = int.Parse(line);
+            current += value;
+        }
+        max = Math.Max(current, max);
+
+        TestContext.WriteLine($"{max}");
     }
 
     [TestMethod]
     public void Part2()
     {
-        var input = DayInput();
-        TestContext.WriteLine($"{"result"}");
+        var result = Calories().OrderByDescending(i => i).Take(3).Sum();
+
+        TestContext.WriteLine($"{result}");
     }
+
+    private IEnumerable<int> Calories()
+    {
+        var input = DayInput();
+        var stringReader = new StringReader(input);
+        var current = 0;
+        while (stringReader.ReadLine() is { } line)
+        {
+            if (line == "")
+            {
+                yield return current;
+                current = 0;
+                continue;
+            }
+
+            var value = int.Parse(line);
+            current += value;
+        }
+
+        yield return current;
+    }
+
+    private const string TestInput = @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+    
 }
