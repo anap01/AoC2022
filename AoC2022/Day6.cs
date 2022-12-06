91488c3fa4d6acdc0476c6bebe891aa5c9399f46
Andreas Appelros <andreas.appelros@gmail.com>
2022-12-06 06:11:56 +0100

    Day 6

--- AoC2022/Day6.cs
+++ AoC2022/Day6.cs
@@ -6,18 +6,48 @@ public class Day6 : AoCTestClass
     [TestMethod]
     public void Part1()
     {
-        var input = DayInput.EnumerateLines();
-        input = TestInput.EnumerateLines();
-        TestContext.Write($"");
+        var input = DayInput;
+        var list = new List<char>();
+        foreach (var (c, i) in input.Select((c, i) => (c, i)))
+        {
+            list.Add(c);
+            if (list.Count == 4)
+            {
+                if (list.ToHashSet().Count == 4)
+                {
+                    TestContext.Write($"{i + 1}");
+                    break;
+                }
+                list.RemoveAt(0);
+            }
+        }
     }
-    
+
     [TestMethod]
     public void Part2()
     {
-        var input = DayInput.EnumerateLines();
-        input = TestInput.EnumerateLines();
-        TestContext.Write($"");    }
-    
-    private const string TestInput = @"";
+        var input = DayInput;
+        var list = new List<char>();
+        foreach (var (c, i) in input.Select((c, i) => (c, i)))
+        {
+            list.Add(c);
+            if (list.Count == 14)
+            {
+                if (list.ToHashSet().Count == 14)
+                {
+                    TestContext.Write($"{i + 1}");
+                    break;
+                }
+
+                list.RemoveAt(0);
+            }
+        }
+    }
+
+    private const string TestInput1 = @"mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    private const string TestInput2 = @"bvwbjplbgvbhsrlpgdmjqwftvncz";
+    private const string TestInput3 = @"nppdvjthqldpwncqszvftbrmjlhg";
+    private const string TestInput4 = @"mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    private const string TestInput5 = @"mjqjpqmgbljsphdztnvjfqwrcgsmlb";
     
 }
